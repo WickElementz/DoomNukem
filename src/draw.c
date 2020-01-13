05ae99616a551c684e9e70270c1ebbd4a3749c66
Yannis Labidi <yalabidi@z1r9p2.le-101.fr>
2020-01-13 13:00:20 +0100

    [edit] draw.c

--- src/draw.c
+++ src/draw.c
@@ -19,35 +19,38 @@ static void		put_pxl(t_env *env, int x, int y, unsigned int color)
 		env->data_ptr[y * WIN_WIDTH + x] = color;
 }
 
-static void		draw(t_env *env, float view[3], int (*xy)[3])
+static void		draw(t_env *env, float view[3], int (*xy)[2])
 {
 	int				wall;
 	int				cmpt;
 	unsigned int	color;
-	int mrg;
+	int				 mrg;
 
 	wall = (64 / view[0]) * ((WIN_WIDTH / 2) / tan(FOV / 2 * M_PI / 180));
 	cmpt = (wall <= WIN_HEIGHT) ? 0 : wall / 2 - WIN_HEIGHT / 2;
-	xy[1] = (wall <= WIN_HEIGHT) ? (WIN_HEIGHT - wall) / 2 : 0;
-	mrg = wall + xy[1];
-	while (xy[1] < WIN_HEIGHT && cmpt < wall)
+	(*xy)[1] = (wall <= WIN_HEIGHT) ? (WIN_HEIGHT - wall) / 2 : 0;
+	mrg = wall + (*xy)[1];
+	while ((*xy)[1] < WIN_HEIGHT && cmpt < wall)
 	{
 		ft_memcpy(&color, &env->text[(int)view[2]].data[(((int)view[1]) +
 					64 * (64 * cmpt / wall)) * 4], sizeof(int));
-		put_pxl(env, xy[0], xy[1]++, color);
+		put_pxl(env, (*xy)[0], (*xy)[1]++, color);
 		cmpt++;
 	}
-	while (xy[1] < WIN_HEIGHT)
+	while ((*xy)[1] < WIN_HEIGHT)
 	{
-		put_pxl(env, xy[0], xy[1], 0x95671F);
-		put_pxl(env, xy[0], xy[1] - mrg, 0x308FC9);
-		xy[1]++;
+		put_pxl(env, (*xy)[0], (*xy)[1], 0x95671F);
+		put_pxl(env, (*xy)[0], (*xy)[1] - mrg, 0x308FC9);
+		(*xy)[1]++;
 	}
 }
 
 void			draw_column(t_env *env, float view[WIN_WIDTH][3])
 {
+	int		xy[2];
 //	dprintf(1, "%f\n", env->cam.angle);
+	xy[0] = -1;
+	xy[0] = 0;
 	while (++xy[0] < WIN_WIDTH)
 	{
 		xy[1] = 0;
